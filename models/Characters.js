be10bdf720cbd93435f87783ee5018b04f8d1a59
Matthew <matthew.petschl@gmail.com>
2019-05-14 21:14:48 -0500

    will not ruin every thing

--- models/Characters.js
+++ models/Characters.js
@@ -3,11 +3,17 @@ module.exports = function(sequelize, DataTypes) {
 var Characters = sequelize.define("Characters", {
   routeName: DataTypes.STRING,
   name: DataTypes.STRING,
+  class: DataTypes.STRING,
+  race: DataTypes.STRING,
+  alignment: DataTypes.STRING,
+  level: DataTypes.INTEGER,
+  experiene: DataTypes.INTEGER,
+  speed: DataTypes.INTEGER,
   charisma: DataTypes.INTEGER,
   strength: DataTypes.INTEGER,
   dexterity: DataTypes.INTEGER,
   constitution: DataTypes.INTEGER,
-  intelligence: DataTypes.INTEGER,
+  intelligence: DataTypes.INTEGER
   });
 
   Characters.associate = function(models) {
